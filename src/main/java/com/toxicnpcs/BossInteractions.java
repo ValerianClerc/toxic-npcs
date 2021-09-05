f8486db9d4ae1253b2a9b49781f3c23fd268dd66
Valerian Clerc <valerian.clerc@gmail.com>
2021-09-04 18:47:03 -0700

    Add more bosses + chat lines

--- src/main/java/com/toxicnpcs/BossInteractions.java
+++ src/main/java/com/toxicnpcs/BossInteractions.java
@@ -6,11 +6,45 @@ import net.runelite.api.NpcID;
 import java.util.*;
 
 enum BossInteractions {
-    // Bosses with unique interactions
+    // God wars
+    GENERAL_GRAARDOR_1(NpcID.GENERAL_GRAARDOR, 56, new HashMap<>()),
+    GENERAL_GRAARDOR_2(NpcID.GENERAL_GRAARDOR_6494, 56, new HashMap<>()),
+    KRIL_TSUTSAROTH_1(NpcID.KRIL_TSUTSAROTH, 45, new HashMap<>()),
+    KRIL_TSUTSAROTH_2(NpcID.KRIL_TSUTSAROTH_6495, 45, new HashMap<>()),
+    KREEARRA_1(NpcID.KREEARRA, 50, new HashMap<>()),
+    KREEARRA_2(NpcID.KREEARRA_6492, 50, new HashMap<>()),
+    COMMANDER_ZILYANA_1(NpcID.COMMANDER_ZILYANA, 25, new HashMap<>()),
+    COMMANDER_ZILYANA_2(NpcID.COMMANDER_ZILYANA_6493, 25, new HashMap<>()),
+    // Wildy bosses
+    CALLISTO_1(NpcID.CALLISTO, 50, new HashMap<>()),
+    CALLISTO_2(NpcID.CALLISTO_6609, 50, new HashMap<>()),
+    VENENATIS_1(NpcID.VENENATIS, 47, new HashMap<>()),
+    VENENATIS_2(NpcID.VENENATIS_6610, 47, new HashMap<>()),
+    VETION(NpcID.VETION, 42, new HashMap<>()),
+    // Other
     ZULRAH_1(NpcID.ZULRAH, 40, RoastData.ZULRAH_ROASTS),
     ZULRAH_2(NpcID.ZULRAH_2043, 40, RoastData.ZULRAH_ROASTS),
     ZULRAH_3(NpcID.ZULRAH_2044, 40, RoastData.ZULRAH_ROASTS),
-    DEFAULT(-1, 80, new HashMap<>());
+    CORPOREAL_BEAST(NpcID.CORPOREAL_BEAST, 49, new HashMap<>()),
+    // Raids 1
+    TEKTON_1(NpcID.TEKTON, 48, new HashMap<>()),
+    TEKTON_2(NpcID.TEKTON_7541, 48, new HashMap<>()),
+    TEKTON_3(NpcID.TEKTON_7542, 48, new HashMap<>()),
+    TEKTON_4(NpcID.TEKTON_7545, 48, new HashMap<>()),
+    TEKTON_5(NpcID.TEKTON_ENRAGED, 55, new HashMap<>()),
+    TEKTON_6(NpcID.TEKTON_ENRAGED_7544, 55, new HashMap<>()),
+    MUTTADILE_1(NpcID.MUTTADILE, 60, new HashMap<>()),
+    MUTTADILE_2(NpcID.MUTTADILE_7562, 60, new HashMap<>()),
+    MUTTADILE_3(NpcID.MUTTADILE_7563, 60, new HashMap<>()),
+    OLM_1(NpcID.GREAT_OLM, 40, new HashMap<>()),
+    OLM_2(NpcID.GREAT_OLM_7554, 40, new HashMap<>()),
+    // Raids 2
+    PESTILENT_BLOAT_1(NpcID.PESTILENT_BLOAT, 60, new HashMap<>()),
+    PESTILENT_BLOAT_2(NpcID.PESTILENT_BLOAT_10812, 60, new HashMap<>()),
+    PESTILENT_BLOAT_3(NpcID.PESTILENT_BLOAT_10813, 60, new HashMap<>()),
+    PESTILENT_BLOAT_4(NpcID.PESTILENT_BLOAT_11184, 60, new HashMap<>()),
+    // Default
+    DEFAULT(-1, 45, new HashMap<>());
 
     private static final Map<Integer, BossInteractions> bosses;
     private final int id;
--- src/main/java/com/toxicnpcs/RoastData.java
+++ src/main/java/com/toxicnpcs/RoastData.java
@@ -4,11 +4,12 @@ import java.util.*;
 
 public class RoastData {
     public static final Map<RoastType, List<String>> DEFAULT_IN_COMBAT_ROASTS = new HashMap<RoastType, List<String>>() {{
-        put(CombatRoastType.DEATH, new ArrayList<>(Arrays.asList("Sit.", "????", "u good?", "l0000000000l")));
-        put(CombatRoastType.HEAVY_DAMAGE, new ArrayList<>(Collections.singletonList("hE nEeD SOmE mIlK")));
+        put(CombatRoastType.DEATH, new ArrayList<>(Arrays.asList("Sit.", "????", "l0000000000l", "Gf noob", "Cya in lumby", "00f")));
+        put(CombatRoastType.HEAVY_DAMAGE, new ArrayList<>(Arrays.asList("u good?", "safe up bro", "Check this fat xp drop")));
     }};
 
     public static final Map<RoastType, List<String>> ZULRAH_ROASTS = new HashMap<RoastType, List<String>>() {{
         put(CombatRoastType.DEATH, new ArrayList<>(Collections.singletonList("jajajajajaja")));
+        put(CombatRoastType.HEAVY_DAMAGE, new ArrayList<>(Collections.singletonList("jajajajajaja")));
     }};
 }
